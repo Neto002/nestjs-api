0b45ca9d5efb78a946dc34ddc78c0b36e1334f23
Neto002 <antoniogfneto92@hotmail.com>
2022-03-30 10:51:29 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [], // Registrar outros módulos
+  imports: [OrdersModule], // Registrar outros módulos
   controllers: [AppController], // Registrar os controladores
   providers: [AppService],
 })
